332f3b4e236e2be9271f6ae40b8919513b8ce303
youngtak <diabloss9999@gmail.com>
2026-01-16 09:28:19 +0900

    [프로그래머스] L0 - 나머지 구하기 (#5)

---
+++ programmers/L0_기초입문/입문/005_나머지_구하기.py
@@ -0,0 +1,12 @@
+# 나머지 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120810
+# 알고리즘: 기초
+# 작성자: 최영탁
+# 작성일: 2026. 01. 16. 09:28:18
+
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+print(solution(3,2))
